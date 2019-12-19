4067fc2a828b8b72877b14caa862c193d07633c2
Sergei Komarov <komarovsmail33@gmail.com>
2019-12-19 15:24:16 +0300

    add curren user check

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import Routes from 'routes'
 import TopBar from 'components/topBar'
 import {CurrentUserProvider} from 'contexts/currentUser'
+import CurrentUserChecker from 'components/currentUserChecker'
 
 const App = () => {
   return (
     <CurrentUserProvider>
-      <Router>
-        <TopBar />
-        <Routes />
-      </Router>
+      <CurrentUserChecker>
+        <Router>
+          <TopBar />
+          <Routes />
+        </Router>
+      </CurrentUserChecker>
     </CurrentUserProvider>
   )
 }
